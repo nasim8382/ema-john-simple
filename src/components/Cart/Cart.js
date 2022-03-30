a9975d2b76aa9babb67049884ec74e7454a630d4
Nasim Reza <nasim8382@gmail.com>
2022-03-30 14:07:04 +0600

    locat storage added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,16 +4,26 @@ import { faArrowRight, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import './Cart.css'
 
 const Cart = ({cart}) => {
+    let totalPrice = 0;
+    let shipping = 0;
+    let quantity = 0;
+    for (const product of cart) {
+        quantity = quantity + product.quantity;
+        totalPrice = totalPrice + product.price * product.quantity;
+        shipping = shipping + product.shipping;
+    }
+    const tax = parseFloat((totalPrice * 0.1).toFixed(2));
+    const grandTotal = (totalPrice + shipping + tax).toFixed(2);
     return (
         <div className='cart-summary'>
             <h2>Order Summary</h2>
                 <div className='order-text'>
-                    <p>Selected Items: {cart.length}</p>
-                    <p>Total Price: ${}</p>
-                    <p>Total Shipping Charge: ${}</p>
-                    <p>Tax: ${}</p>
+                    <p>Selected Items: {quantity}</p>
+                    <p>Total Price: ${totalPrice}</p>
+                    <p>Total Shipping Charge: ${shipping}</p>
+                    <p>Tax: ${tax}</p>
                 </div>
-                <h3>Grand Total: ${}</h3>
+                <h3>Grand Total: ${grandTotal}</h3>
                 <div className='cart-button'>
                     <button className='clear-btn'>
                         <div className='clear-btn-div'>
